d492c7c4e919f985fd0fb20198d00987e0181745
LinasMaziukas <>
2022-05-26 19:29:01 +0300

    add compiled css/js; redirect main page to pip boy;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,48 +11,7 @@
 </head>
 <body>
 <div class="container">
-    <div class="screen">
-        <div class="screen-reflection"></div>
-        <div class="scan"></div>
-        <nav>
-            <div class="border-bottom">
-                <span class="active stat"></span>
-                <span class="inv"></span>
-                <span class="data"></span>
-                <span class="map"></span>
-                <span class="radio"></span>
-            </div>
-            <div class="">
-                <p>
-                    <span class="status"></span>
-                    <span class="off special"></span>
-                </p>
-            </div>
-        </nav>
-        <div class="vaultboy">
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
-            <div class="bar4"></div>
-            <div class="bar5"></div>
-            <div class="bar6"></div>
-        </div>
-        <div class="supplies"><span>Stimpak (0)</span><span>Radaway (0)</span><span>Stix</span>
-        </div>
-        <div class="info-bar">
-            <span class="weapon"></span>
-            <span class="aim"><p>21</p></span>
-            <span class="helmet"></span>
-            <span class="shield"><p>110</p></span>
-            <span class="voltage"><p>126</p></span>
-            <span class="nuclear"><p>35</p></span>
-        </div>
-        <div class="hud-bar">
-            <div class="hp"></div>
-            <div class="exp"></div>
-            <div class="ap"></div>
-        </div>
-    </div>
+
 </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(\route('stats'));
 });
 
 Route::get('/stats', function () {
